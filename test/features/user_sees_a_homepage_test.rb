7432fd9553f5c9921ac0e4cb849937f32ed863cc
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-25 15:26:02 -0600

    adds index and error views

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_gets_error_when_visiting_a_page_that_doesnt_exist
+    visit '/doesnt_exist'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
 end
