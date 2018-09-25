c2803e5b749d69f3e7ddab76a051624ca0a8deaf
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-25 16:03:08 -0600

    final commit before heroku master push

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -11,19 +11,28 @@ class PersonalSite
     end
   end
 
-    def self.index
-      render_view('index.html')
-    end
+  def self.index
+    render_view('index.html')
+  end
 
-    def self.about
-      render_view('about.html')
-    end
+  def self.about
+    render_view('about.html')
+  end
 
-    def self.error
-      render_view('error.html', '404')
-    end
+  def self.error
+    render_view('error.html', '404')
+  end
 
-    def self.render_view(page, code = '200')
-      [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
-    end
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
+  end
+
+  def self.css
+    render_static('main.css')
+  end
+
+  def self.render_static(asset)
+    [200, {'Content-Type' => 'text/html'}, [File.read("./public/#{asset}")]]
+  end
+  
 end
